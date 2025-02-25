a46d69b70deaa68df065c16921f3fecd10c00d04
Stephanie Armbruster <stephaniearmbruster@dhcp-10-250-16-222.harvard.edu>
2025-02-25 17:17:59 -0500

    Include repetitions

--- Simulation/Standard Methods/RouanetSimulation.R
+++ Simulation/Standard Methods/RouanetSimulation.R
@@ -2,6 +2,7 @@
 
 library(tidyverse)
 library(MASS)
+library(rlist)
 
 # packages for generalized linear mixed models
 library(nlme)
@@ -26,6 +27,7 @@ RouanetSim <- function(n, tij,
 
   Y.long <- array(NA, dim = c(n, length(tij)))
 
+  # random effects
   u.i <- mvrnorm(n = n, mu = c(0, 0),
                  Sigma = matrix(c(0.3, -0.1, -0.1, 0.1), byrow = T,
                                 nrow = 2))
@@ -34,7 +36,6 @@ RouanetSim <- function(n, tij,
   for (i in seq(1, n)) {
     Y.long[i, ] <- 20 - 0.3 * tij + u.i[i, 1] + u.i[i, 2] * tij + rnorm(1, mean = 0, sd = 0.9)
   }
-
 
   # non-terminal event / terminal event and censoring
   risk.T <- array(1, dim = c(n, length(tij)))
@@ -49,6 +50,7 @@ RouanetSim <- function(n, tij,
 
   for (i in seq(1, n)) {
 
+    # generate time of event for DNAR scenario
     if (DNAR) {
       hazard <- gamma[1] * exp(gamma[2] * X[i] + gamma[3] * u.i[i, 2])
       EventTime <- -log(runif(1)) / hazard
@@ -58,34 +60,36 @@ RouanetSim <- function(n, tij,
       # current time
       time <- tij[t]
 
+      # is patient currently at risk?
       it.risk.C <- risk.C[i, t - 1]
       it.risk.T <- risk.T[i, t - 1]
 
-
-      censProb <- eta[1] + eta[2] * X[i] + eta[3] * Y.long[i, t - 1]
+      # estimate linear predictor for event and censoring probability
       eventProb <- gamma[1] + gamma[2] * X[i] + gamma[3] * Y.long[i, t - 1]
+      censProb <- eta[1] + eta[2] * X[i] + eta[3] * Y.long[i, t - 1]
 
       weight.C[i, t] <- exp(censProb) / (1 + exp(censProb))
       weight.CT[i, t] <- (exp(censProb) / (1 + exp(censProb))) * (exp(eventProb) / (1 + exp(eventProb)))
 
-      # if not censored so far
+      # if currently at risk
       if (it.risk.T == 1) {
         if (DNAR == F) {
-          # censoring this time
+          # event this time; model defined in terms of survival probability
           Y.T[i, t] <- rbinom(n = 1, size = 1,
                               prob = 1 - exp(eventProb) / (1 + exp(eventProb)))
         } else {
           Y.T[i, t] <- (EventTime <= time)
         }
 
-        # not at risk for death in future
+        # not at risk for event or censoring in future, once event happened
         if (Y.T[i, t] == 1) {
           risk.T[i, t] <- 0
           risk.C[i, t] <- 0
         }
 
-        # if not dead, at risk for censoring
+        # if event not happened and at risk for censoring
         if (Y.T[i, t] == 0 & it.risk.C == 1) {
+          # censoring this time; model defined in terms of survival probability
           Y.C[i, t] <- rbinom(n = 1, size = 1,
                                prob = 1 - exp(censProb) / (1 + exp(censProb)))
           if (Y.C[i, t] == 1) {
@@ -181,195 +185,234 @@ RouanetSim <- function(n, tij,
 }
 
 
-# Data simulation ---------------------------------------------------------
-
-dataDARMAR <- RouanetSim(n = 1000,
-                         tij = c(0, 4, 12, 16, 20),
-                         eta = c(-0.5, 0.5, 0.15),
-                         gamma = c(0.5, 0.5, 0.1),
-                         DNAR = F) %>%
-  group_by(id) %>%
-  arrange(time) %>%
-  mutate(MarkerLag = c(0, na.omit(lag(Marker)))) %>%
-  ungroup()
-
-# observed data
-dataDARMAR.CT <- dataDARMAR %>%
-  group_by(id) %>%
-  mutate(CTInd = ifelse(Event == 1 | Cens == 1, 1, 0)) %>%
-  filter(cumsum(CTInd) <= 1) %>%
-  # filter(CTInd != 1) %>%
-  ungroup() %>%
-  arrange(id, time) %>%
-  as.data.frame()
+# save predictions
+DARMAR.RES <- list()
+DNARMAR.RES <- list()
 
 
+j <- 1
+r <- 1
 
-dataDNARMAR <- RouanetSim(n = 1000,
-                          tij = c(0, 4, 12, 16, 20),
-                          eta = c(-1, 1, 0.2),
-                          gamma = c(0.05, -1, -1),
-                          DNAR = T) %>%
-  group_by(id) %>%
-  arrange(time) %>%
-  mutate(MarkerLag = c(0, na.omit(lag(Marker)))) %>%
-  ungroup()
+while (r <= 500) {
 
-# observed data
-dataDNARMAR.CT <- dataDNARMAR %>%
-  group_by(id) %>%
-  mutate(CTInd = ifelse(Event == 1 | Cens == 1, 1, 0)) %>%
-  filter(cumsum(CTInd) <= 1) %>%
-  # filter(CTInd != 1) %>%
-  ungroup() %>%
-  arrange(id, time) %>%
-  as.data.frame()
+  # Data simulation ---------------------------------------------------------
+  # set seed
+  set.seed(j)
+
+  dataDARMAR <- RouanetSim(n = 500,
+                           tij = c(0, 4, 12, 16, 20),
+                           eta = c(-0.5, 0.5, 0.15),
+                           gamma = c(0.5, 0.5, 0.1),
+                           DNAR = F) %>%
+    group_by(id) %>%
+    arrange(time) %>%
+    mutate(MarkerLag = c(0, na.omit(lag(Marker)))) %>%
+    ungroup()
+
+  # observed data: observed as long as not truncated by event or censoring
+  dataDARMAR.CT <- dataDARMAR %>%
+    group_by(id) %>%
+    mutate(CTInd = ifelse(Event == 1 | Cens == 1, 1, 0)) %>%
+    filter(cumsum(CTInd) <= 1) %>%
+    ungroup() %>%
+    arrange(id, time) %>%
+    as.data.frame()
 
 
 
-# Models ------------------------------------------------------------------
-# linear mixed models
-LMM.DARMAR <- lmer(Marker ~ X + time + I(time^2) + I(X * time) + I(X * time^2) + (1 | id),
-                   data = dataDARMAR.CT)
+  dataDNARMAR <- RouanetSim(n = 1000,
+                            tij = c(0, 4, 12, 16, 20),
+                            eta = c(-1, 1, 0.2),
+                            gamma = c(0.05, -1, -1),
+                            DNAR = T) %>%
+    group_by(id) %>%
+    arrange(time) %>%
+    mutate(MarkerLag = c(0, na.omit(lag(Marker)))) %>%
+    ungroup()
 
-LMM.DNARMAR <- lmer(Marker ~ X + time + I(time^2) + I(X * time) + I(X * time^2) + (1 | id),
-                    data = dataDNARMAR.CT)
+  # observed data: observed as long as not truncated by event or censoring
+  dataDNARMAR.CT <- dataDNARMAR %>%
+    group_by(id) %>%
+    mutate(CTInd = ifelse(Event == 1 | Cens == 1, 1, 0)) %>%
+    filter(cumsum(CTInd) <= 1) %>%
+    ungroup() %>%
+    arrange(id, time) %>%
+    as.data.frame()
 
-# IEE
-IEE.DARMAR <- geeglm(Marker ~ X + time + I(time^2) + I(X * time) + I(X * time^2),
-                   id = id,
-                   data = dataDARMAR.CT)
-IEE.DNARMAR <- geeglm(Marker ~ X + time + I(time^2) + I(X * time) + I(X * time^2),
-                    id = id,
-                    data = dataDNARMAR.CT)
 
-# pooled regression for weights
-CT.Weights.DARMAR <- glm(CTInd ~ X + MarkerLag,
-                         data = dataDARMAR.CT,
-                         family = binomial)
-dataDARMAR.CT <- dataDARMAR.CT %>%
-  mutate(prob.CT = predict(CT.Weights.DARMAR,
+
+  # Models ------------------------------------------------------------------
+  # linear mixed models
+  LMM.DARMAR <- lmer(Marker ~ X + time + I(time^2) + I(X * time) + I(X * time^2) + (1 | id),
+                     data = dataDARMAR.CT)
+
+  LMM.DNARMAR <- lmer(Marker ~ X + time + I(time^2) + I(X * time) + I(X * time^2) + (1 | id),
+                      data = dataDNARMAR.CT)
+
+  # IEE
+  IEE.DARMAR <- geeglm(Marker ~ X + time + I(time^2) + I(X * time) + I(X * time^2),
+                       id = id,
+                       data = dataDARMAR.CT)
+  IEE.DNARMAR <- geeglm(Marker ~ X + time + I(time^2) + I(X * time) + I(X * time^2),
+                        id = id,
+                        data = dataDNARMAR.CT)
+
+  # pooled regression for weights
+  CT.Weights.DARMAR <- glm(CTInd ~ X + MarkerLag,
+                           data = dataDARMAR.CT,
+                           family = binomial)
+  dataDARMAR.CT <- dataDARMAR.CT %>%
+    mutate(prob.CT = predict(CT.Weights.DARMAR,
                              type = "response")) %>%
-  group_by(id) %>%
-  arrange(time) %>%
-  mutate(weight.CT = cumprod(1 - prob.CT)) %>%
-  ungroup()
+    group_by(id) %>%
+    arrange(time) %>%
+    mutate(weight.CT = cumprod(1 - prob.CT)) %>%
+    ungroup()
 
-CT.Weights.DNARMAR <- glm(CTInd ~ X + MarkerLag,
-                          data = dataDNARMAR.CT,
+  CT.Weights.DNARMAR <- glm(CTInd ~ X + MarkerLag,
+                            data = dataDNARMAR.CT,
+                            family = binomial)
+  dataDNARMAR.CT <- dataDNARMAR.CT %>%
+    mutate(prob.CT = predict(CT.Weights.DNARMAR,
+                             type = "response")) %>%
+    group_by(id) %>%
+    arrange(time) %>%
+    mutate(weight.CT = cumprod(1 - prob.CT)) %>%
+    ungroup()
+
+  C.Weights.DARMAR <- glm(Cens ~ X + MarkerLag,
+                          data = dataDARMAR.CT,
                           family = binomial)
-dataDNARMAR.CT <- dataDNARMAR.CT %>%
-  mutate(prob.CT = predict(CT.Weights.DNARMAR,
-                           type = "response")) %>%
-  group_by(id) %>%
-  arrange(time) %>%
-  mutate(weight.CT = cumprod(1 - prob.CT)) %>%
-  ungroup()
 
-C.Weights.DARMAR <- glm(Cens ~ X + MarkerLag,
-                        data = dataDARMAR.CT,
-                        family = binomial)
+  dataDARMAR.CT <- dataDARMAR.CT %>%
+    mutate(prob.C = predict(C.Weights.DARMAR,
+                            type = "response")) %>%
+    group_by(id) %>%
+    arrange(time) %>%
+    mutate(weight.C = cumprod(1 - prob.C)) %>%
+    ungroup()
 
-dataDARMAR.CT <- dataDARMAR.CT %>%
-  mutate(prob.C = predict(C.Weights.DARMAR,
-                           type = "response")) %>%
-  group_by(id) %>%
-  arrange(time) %>%
-  mutate(weight.C = cumprod(1 - prob.C)) %>%
-  ungroup()
+  C.Weights.DNARMAR <- glm(Cens ~ X + MarkerLag,
+                           data = dataDNARMAR.CT,
+                           family = binomial)
+  dataDNARMAR.CT <- dataDNARMAR.CT %>%
+    mutate(prob.C = predict(C.Weights.DNARMAR,
+                            type = "response")) %>%
+    group_by(id) %>%
+    arrange(time) %>%
+    mutate(weight.C = cumprod(1 - prob.C)) %>%
+    ungroup()
 
-C.Weights.DNARMAR <- glm(Cens ~ X + MarkerLag,
-                         data = dataDNARMAR.CT,
-                         family = binomial)
-dataDNARMAR.CT <- dataDNARMAR.CT %>%
-  mutate(prob.C = predict(C.Weights.DNARMAR,
-                          type = "response")) %>%
-  group_by(id) %>%
-  arrange(time) %>%
-  mutate(weight.C = cumprod(1 - prob.C)) %>%
-  ungroup()
+  # GEE with drop-out and event probability
+  GEE.CT.DARMAR <- geeglm(Marker ~ X + time + I(time^2) + I(X * time) + I(X * time^2),
+                          id = id,
+                          data = dataDARMAR.CT,
+                          weights = 1 / weight.CT)
+  GEE.CT.DNARMAR <- geeglm(Marker ~ X + time + I(time^2) + I(X * time) + I(X * time^2),
+                           id = id,
+                           data = dataDNARMAR.CT,
+                           weights = 1 / weight.CT)
 
-# GEE with drop-out and event probability
-GEE.CT.DARMAR <- geeglm(Marker ~ X + time + I(time^2) + I(X * time) + I(X * time^2),
-                        id = id,
-                        data = dataDARMAR.CT,
-                        weights = 1 / weight.CT)
-GEE.CT.DNARMAR <- geeglm(Marker ~ X + time + I(time^2) + I(X * time) + I(X * time^2),
+
+  # GEE with drop-out
+  GEE.C.DARMAR <- geeglm(Marker ~ X + time + I(time^2) + I(X * time) + I(X * time^2),
                          id = id,
-                         data = dataDNARMAR.CT,
-                         weights = 1 / weight.CT)
-
-
-# GEE with drop-out
-GEE.C.DARMAR <- geeglm(Marker ~ X + time + I(time^2) + I(X * time) + I(X * time^2),
-                        id = id,
-                        data = dataDARMAR.CT,
-                        weights = 1 / weight.C)
-GEE.C.DNARMAR <- geeglm(Marker ~ X + time + I(time^2) + I(X * time) + I(X * time^2),
-                         id = id,
-                         data = dataDNARMAR.CT,
+                         data = dataDARMAR.CT,
                          weights = 1 / weight.C)
+  GEE.C.DNARMAR <- geeglm(Marker ~ X + time + I(time^2) + I(X * time) + I(X * time^2),
+                          id = id,
+                          data = dataDNARMAR.CT,
+                          weights = 1 / weight.C)
 
 
-# Joint model
-LME.DNARMAR <- lme(Marker ~ X,
-                   random = ~ time | id,
-                   data = dataDNARMAR.CT,
-                   method = "REML")
+  # Joint model
+  LME.DNARMAR <- lme(Marker ~ X + time + I(time^2) + I(X * time) + I(X * time^2),
+                     random = ~ 1 | id,
+                     data = dataDNARMAR.CT,
+                     method = "REML")
 
-dataDNARMAR.CT.UNQ <- rbind(dataDNARMAR.CT %>%
-                             group_by(id) %>%
-                             filter(all(Event == 0)) %>%
-                             filter(time == max(time)),
-                           dataDNARMAR.CT %>%
-                             group_by(id) %>%
-                             filter(Event == 1))
+  dataDNARMAR.CT.UNQ <- rbind(dataDNARMAR.CT %>%
+                                group_by(id) %>%
+                                filter(all(Event == 0)) %>%
+                                filter(time == max(time)),
+                              dataDNARMAR.CT %>%
+                                group_by(id) %>%
+                                filter(Event == 1))
 
-SURV.DNARMAR <- coxph(Surv(time, Event) ~ X + MarkerLag,
-                      data = dataDNARMAR.CT.UNQ,
-                      x = TRUE)
-dForm <- list(fixed = ~ 1 + I(2 * time) + X + I(2 * X * time),
-              indFixed = c(3, 4, 5, 6),
-              random = ~ 1,
-              indRandom = 1)
+  SURV.DNARMAR <- coxph(Surv(time, Event) ~ X + MarkerLag,
+                        data = dataDNARMAR.CT.UNQ,
+                        x = TRUE)
+  dForm <- list(fixed = ~ 1 + I(2 * time) + X + I(2 * X * time),
+                indFixed = c(3, 4, 5, 6),
+                random = ~ 1,
+                indRandom = 1)
 
-JM.DNARMAR <- jointModel(LME.DNARMAR, SURV.DNARMAR,
-                        timeVar = "time",
-                        parameterization = "slope",
-                        derivForm = dForm)
+  # JM.DNARMAR <- jointModel(LME.DNARMAR, SURV.DNARMAR,
+  #                          timeVar = "time",
+  #                          parameterization = "slope",
+  #                          derivForm = dForm)
+  JM.DNARMAR <- tryCatch({
+    JM.DNARMAR <- jointModel(LME.DNARMAR, SURV.DNARMAR,
+                             timeVar = "time",
+                             parameterization = "slope",
+                             derivForm = dForm)
+    JM.DNARMAR  # Return the successful result
+  }, error = function(e) {
+    message("Error encountered at index ", i, ": ", e$message)
+    return(NULL)  # Return NULL and move to the next iteration
+  })
+
+  if (is.null(JM.DNARMAR)) {
+    print(j)
+    j <- j + 1
+    next  # Skip to the next iteration of the loop
+  }
+
+  # Estimation --------------------------------------------------------------
+  DARMAR.RES[[r]] <- data.frame(time = dataDARMAR$time,
+                                X = dataDARMAR$X,
+
+                                LMM = predict(LMM.DARMAR, newdata = dataDARMAR),
+                                IEE = predict(IEE.DARMAR, newdata = dataDARMAR),
+                                GEE.CT = predict(GEE.CT.DARMAR, newdata = dataDARMAR),
+                                GEE.C = predict(GEE.C.DARMAR, newdata = dataDARMAR))
+
+  DNARMAR.RES[[r]] <- data.frame(time = dataDNARMAR$time,
+                                 X = dataDNARMAR$X,
+
+                                 LMM = predict(LMM.DNARMAR, newdata = dataDNARMAR),
+                                 IEE = predict(IEE.DNARMAR, newdata = dataDNARMAR),
+                                 GEE.CT = predict(GEE.CT.DNARMAR, newdata = dataDNARMAR),
+                                 GEE.C = predict(GEE.C.DNARMAR, newdata = dataDNARMAR),
+                                 JM = predict(JM.DNARMAR, newdata = dataDNARMAR))
+
+  # move index
+  print(r)
+  r <- r + 1
+
+}
 
 
 
-
-# Estimation --------------------------------------------------------------
-dataDARMAR$LMM <- predict(LMM.DARMAR, newdata = dataDARMAR)
-dataDNARMAR$LMM <- predict(LMM.DNARMAR, newdata = dataDNARMAR)
-
-dataDARMAR$IEE <- predict(IEE.DARMAR, newdata = dataDARMAR)
-dataDNARMAR$IEE <- predict(IEE.DNARMAR, newdata = dataDNARMAR)
-
-dataDARMAR$GEE.CT <- predict(GEE.CT.DARMAR, newdata = dataDARMAR)
-dataDNARMAR$GEE.CT <- predict(GEE.CT.DNARMAR, newdata = dataDNARMAR)
-
-dataDARMAR$GEE.C <- predict(GEE.C.DARMAR, newdata = dataDARMAR)
-dataDNARMAR$GEE.C <- predict(GEE.C.DNARMAR, newdata = dataDNARMAR)
-
-dataDNARMAR$JM <- predict(JM.DNARMAR, newdata = dataDNARMAR)
-
-patients.DARMAR <- dataDARMAR %>%
+# Averaging ---------------------------------------------------------------
+# aggregate over repetitions for each subject
+patients.DARMAR <- DARMAR.RES %>%
+  list.rbind() %>%
   group_by(time, X) %>%
   reframe(LMM = mean(LMM),
           IEE = mean(IEE),
           GEE.CT = mean(GEE.CT),
           GEE.C = mean(GEE.C))
 
-patients.DNARMAR <- dataDNARMAR %>%
+patients.DNARMAR <- DNARMAR.RES %>%
+  list.rbind() %>%
   group_by(time, X) %>%
   reframe(LMM = mean(LMM),
           IEE = mean(IEE),
           GEE.CT = mean(GEE.CT),
           GEE.C = mean(GEE.C),
           JM = mean(JM))
+
 
 # Observed data
 patients.DARMAR <- inner_join(patients.DARMAR,
